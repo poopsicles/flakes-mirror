f83d11ee4dc4e76fc59164bef980ebf00550906a
fumnanya <fmowete@outlook.com>
2024-12-24 16:59:31 +0100

    add common rust linux gui stuff

--- rust/flake.nix
+++ rust/flake.nix
@@ -40,17 +40,23 @@
         formatter = treefmt-nix.lib.mkWrapper pkgs treefmtConfig;
         checks.formatting = (treefmt-nix.lib.evalModule pkgs treefmtConfig).config.build.check self;
 
-        devShells.default = pkgs.mkShell {
+        devShells.default = pkgs.mkShell rec {
           packages = with pkgs; [
             toolchain
             rust-analyzer
             # openssl
             # pkg-config
             # cargo-nextest
+          ] ++ lib.optionals pkgs.stdenv.hostPlatform.isLinux [      
+            # libxkbcommon
+            # xorg.libxcb
+            # wayland
+            # vulkan-loader
           ];
 
           env = {
             RUST_SRC_PATH = "${pkgs.toolchain}/lib/rustlib/src/rust/library";
+            LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath packages}:$LD_LIBRARY_PATH";
           };
         };
       }
